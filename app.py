eaa3799df480f46da34515bca302cd14d357561a
“nathanneuro” <nathan.helm.burger@gmail.com>
2015-10-27 21:20:51 -0400

    Working Stock Ticker graph demo.

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ from flask import Flask, render_template, request, redirect
 import requests
 from bokeh.embed import components
 import bokeh.plotting as plt
+import os
+import pandas as pd
 
 app = Flask(__name__)
 # recommended  Flask(__name__.split('.')[0])
 
-@app.route("/")
+
+# Used https://github.com/andrewyue/dailycandles/blob/master/app.py by Andrew Yue
+# As a reference for trickier parts like this
+def getitem(obj, item, default):
+    if item not in obj:
+        return default
+    else:
+        return obj[item]
+
+
+@app.route('/')
 def main():
     return redirect('/index')
 
@@ -15,21 +27,39 @@ def main():
 def index():
     return render_template('index.html')
 
-stock = 'GOOG'
-api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % stock
-session = requests.Session()
-session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
-raw_data = session.get(api_url)
 
 
-@app.route('/graph')
-def graph():
-    plot = plt.figure(tools=TOOLS,
-                      title='Data from Quandle WIKI set',
-                      x_axis_label='date',
-                      x_axis_type='datetime')
-    script, div = components(plot)
-    return render_template('graph.html', script=script, div=div)
+
+@app.route('/stockgraph')
+def stockgraph():
+    args = request.args
+    ticker = getitem(args, 'ticker', 'GOOG')
+    api_key = 'J57eFVXL_QaU2GkUyLkj'
+    api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/{}.json?api_key={}'. format(ticker, api_key)
+    session = requests.Session()
+    session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+    # raw_data = session.get(api_url)
+    raw = requests.get(api_url)
+    if(raw.json().get('error') is True):  # changed from == unicode
+        # any returned error message means there's uh, an error
+        return render_template('stockerror.html')
+    else:
+        # Raw data to Pandas dataframe
+        df = pd.DataFrame(raw.json().get('data'), columns=raw.json().get('column_names'))
+        df['Date'] = pd.to_datetime(df['Date'])
+        # Making the plot
+        TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
+        plot = plt.figure(tools=TOOLS,
+                          title='Data from Quandle WIKI set',
+                          x_axis_label='date',
+                          x_axis_type='datetime')
+        plot.line(df.Date, df.Open, color='green', legend='Open')
+        plot.line(df.Date, df.Close, color='red', legend='Close')
+
+        plot.yaxis.axis_label = 'Price'
+        script, div = components(plot)
+        return render_template('stockgraph.html', script=script, div=div, stock=ticker.upper())
 
 if __name__ == '__main__':
-    app.run(port=33507)
+    port = int(os.environ.get("PORT", 33507))
+    app.run(host='0.0.0.0',port=port, debug=True)
