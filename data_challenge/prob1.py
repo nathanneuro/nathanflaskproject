09d24ad2b410c58ba0d4c0e077504d95f81dd6f8
“nathanneuro” <nathan.helm.burger@gmail.com>
2015-10-31 12:07:31 -0400

    Walk peak sim added.

--- data_challenge/prob1.py
+++ data_challenge/prob1.py
@@ -11,20 +11,23 @@ from scipy import stats
 import numpy as np
 import itertools
 np.set_printoptions(precision=9)
-
-n = 60  # Steps
+import math
+n = 10  # Steps
 p1 = 0.5 # Probability of vertical or horizontal
 p2 = 0.5 # Probability of success (towards target)
-goal = 10 # How far is the threshold as crow flies?
+goal = 3 # How far is the threshold as crow flies?
 # length = ((x**2) + (y**2))**0.5 # radius length from 0,0
-# q = 100 # trials
+# q = 100 # trials for simulation
 
-def walk_1D(n, p1, p2, goal):
+p = 0.5 # For 1D
+# math.factorial(x)
+def walk_1D(n, p, goal):
     k = np.arange(goal,n + 1)  # Number of successes needed if 1D
     binomial = stats.binom.pmf(k, n, p)
     print(binomial, k)
     print(sum(binomial))
-    return print(format(sum(binomial), '.10g'))
+    print(format(sum(binomial), '.10g'))
+    return sum(binomial)
 
 
 def walk_pmf_add(n, p1, p2, goal):
@@ -49,7 +52,9 @@ def walk_pmf_add(n, p1, p2, goal):
         for y in y_list:
             for x in x_list:
                 if ((x**2 + y**2)**0.5) >= goal:
-                    print ("x", x, "and y", y)
+                    # print ("x", x, "and y", y)
+                    # remember that "losses" count too, so two-tailed
+                    # multiplying by binompmf by 2 > 1 tot p nonsensical
                     binom_x = stats.binom.pmf(x, x_count, p2)
                     binom_y = stats.binom.pmf(y, y_count, p2)
                     prob_mass = prob_mass + (binom_x * binom_y)
@@ -65,13 +70,39 @@ def walk_pmf_add(n, p1, p2, goal):
 
 
 
-print("Cumulative Probability in Total: ", walk_pmf_add(n, p1, p2, goal))
+# print("Cumulative Probability in Total: ", walk_pmf_add(n, p1, p2, goal))
+
+# w = walk_1D(n, p, goal)
 
 
+def walk_sim_peak(trials, n, goal):
+    cross = 0
+    for t in range(trials + 1):
+        x = 0
+        y = 0
+        print("Trial #", t)
+        for i in range(n + 1):
+            move = np.random.randint(0,4)
+            print("Move rand int: ", move, " for step number ", i)
+            if move == 0:
+                x = x - 1
+            elif move == 1:
+                x = x + 1
+            elif move == 2:
+                y = y - 1
+            elif move == 3:
+                y = y + 1
+            else:
+                print("Error. Wrong Move direction.")
+            if ((x**2 + y**2)**0.5) >= goal:
+                cross = cross + 1
+                print("Cross! ", cross)
+                break
+    print("Crosses: ", cross, " Total trials: ", trials, " p = ", cross/trials)
+    return (cross, trials, cross/trials)
 
 
-
-
+walk_sim_peak(3, 10, 3)
 
 
 #def walkendpoint(n, p1, q, p2):
