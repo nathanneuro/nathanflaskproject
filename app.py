df8e2031afcd6d44cb99e1843050590dba223036
“nathanneuro” <nathan.helm.burger@gmail.com>
2015-11-10 09:24:34 -0500

    Maybe.

--- app.py
+++ app.py
@@ -219,5 +219,5 @@ def stockgraph():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 33507))
-    app.run(host='0.0.0.0',port=port, debug=("DEBUG" in os.environ))
+    app.run(host='0.0.0.0',port=port, debug=("DEBUG" in os.environ, False))
     print(os.environ)
