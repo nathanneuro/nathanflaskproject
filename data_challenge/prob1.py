04fcf3236c3316663b360ef19587a2cd12e3f497
“nathanneuro” <nathan.helm.burger@gmail.com>
2016-01-03 20:58:45 -0500

    Added paralellism to data challenge code. Result is a 2x speedup, due to my two CPU cores.

--- data_challenge/prob1.py
+++ data_challenge/prob1.py
@@ -5,34 +5,46 @@
 # from (0,0) to (x,y)
 # length = sqrt((x**2)+(y**2))
 
-from scipy import stats
+#from scipy import stats
 import numpy as np
 import itertools
 np.set_printoptions(precision=9)
 import math
+import timeit
+from functools import partial
+from multiprocessing import Pool
 
-n = 10  # Steps
-p1 = 0.5 # Probability of vertical or horizontal
-p2 = 0.5 # Probability of success (towards target)
-goal = 3 # How far is the threshold as crow flies?
+
+#Parameters
+
+#n = 10  # Steps
+#p1 = 0.5 # Probability of vertical or horizontal
+#p2 = 0.5 # Probability of success (towards target)
+#goal = 3 # How far is the threshold as crow flies?
 # length = ((x**2) + (y**2))**0.5 # radius length from 0,0
 # q = 100 # trials for simulation
 
-p = 0.5 # For 1D
+#p = 0.5 # For 1D
+
+
 # math.factorial(x)
-def walk_1D(n, p, goal):
-    k = np.arange(goal,n + 1)  # Number of successes needed if 1D
-    binomial = stats.binom.pmf(k, n, p)
-    print(binomial, k)
-    print(sum(binomial))
-    print(format(sum(binomial), '.10g'))
-    return sum(binomial)
+#def walk_1D(n, p, goal):
+#    k = np.arange(goal,n + 1)  # Number of successes needed if 1D
+#    binomial = stats.binom.pmf(k, n, p)
+#    print(binomial, k)
+#    print(sum(binomial))
+#    print(format(sum(binomial), '.10g'))
+#    return sum(binomial)
 
 
-
-def walk_sim_peak(trials, n, goal):
+def walk_sim_peak(params):
+    job_start_time = timeit.default_timer()
     cross = 0
     move_tot = 0
+    run_tot = [0, 0, 0, 0, 0]
+    trials = params[0]
+    n = params[1]
+    goal = params[2]
     for t in range(trials):
         x = 0
         y = 0
@@ -48,26 +60,58 @@ def walk_sim_peak(trials, n, goal):
                 y = y - 1
             elif move == 3:
                 y = y + 1
-            else:
-                print("Error. Wrong Move direction.")
+            #else:
+            #    print("Error. Wrong Move direction.")
             # print("(", x, ", ", y, ")")
         if ((x**2 + y**2)**0.5) >= goal:
             cross = cross + 1
             # print("Cross! ", cross)
             move_tot = move_tot + (i+1)
-    print("Crosses: ", cross, " Total trials: ", trials, " p = ", cross/trials, "Move avg:", move_tot/trials)
-    return (cross, trials, cross/trials, move_tot)
+    #print("Crosses: ", cross, " Total trials: ", trials, " p = ", cross/trials, "Move avg:", move_tot/trials)
+    run_tot[0] = run_tot[0] + cross  # crosses
+    run_tot[1] = run_tot[1] + trials  # trials
+    run_tot[2] = run_tot[0] / run_tot[1]  # crosses/trials
+    run_tot[3] = run_tot[3] + move_tot  # moves total
+    run_tot[4] = timeit.default_timer() - job_start_time  # job time elapsed
 
-run_tot = [0, 0, 0, 0]
+    return (run_tot)
 
-for job in range(15):
-    w = walk_sim_peak((10**5)*50, 10, 3)
-    run_tot[0] = run_tot[0] + w[0]
-    run_tot[1] = run_tot[1] + w[1]
-    run_tot[2] = run_tot[0] / run_tot[1]
-    run_tot[3] = run_tot[3] + w[3]
-print("Crosses: ", run_tot[0], "Trials: ", run_tot[1], " p = ", run_tot[2], "Move Avg:", (run_tot[3]/run_tot[1]) )
 
+# def parallel_attribute(f): # this would let it take a func with multiple params
+# Assumes f has one argument unless using partial (confused so not using for now)
+
+def easy_parallize(f, sequence):
+    pool = Pool(processes=2) # Defaults to num of processors (2) if unspecified
+    result = pool.map(f, sequence) # e.g. (function, jobs)
+    cleaned = [x for x in result if not x is None]
+    # cleaned = asarray(cleaned)
+    # not optimal but safe
+    pool.close()
+    pool.join()
+    return cleaned
+
+    # return partial(easy_parallize, f)        
+
+
+def main():
+    total_start_time = timeit.default_timer()
+    # Parameters
+    repetitions = (10**4)*50  # trials to repeat over
+    n = 10  # steps to take
+    goal = 3  # boundary to cross
+    params = [repetitions, n, goal]
+    jobs = [] # list of jobs (param inputs) to run through function
+    jobruns = range(5)  # times to repeat same entire job
+    
+    for j in jobruns:
+        jobs.append(params)
+    
+    results = easy_parallize(walk_sim_peak, jobs)
+    
+    for run_tot in results:
+        print("Crosses: ", run_tot[0], "Trials: ", run_tot[1], " p = ", run_tot[2], "Move Avg:", (run_tot[3]/run_tot[1]), "\nTime elapsed for job: ", run_tot[4], " secs")
+        
+    print("Total time elapsed for all jobs: ", timeit.default_timer() - total_start_time, " secs")
 
 
 def walk_pmf_add(n, p1, p2, goal):
@@ -153,3 +197,5 @@ def ends(walks=1000, n=10, dimensions=2):
 #print(simX, simY, lengths)
 #print(probpass)
 
+if __name__ == '__main__':
+   main()
