dcc867a0d7c0c91c4cead2727212e0afe1964cd5
“nathanneuro” <nathan.helm.burger@gmail.com>
2016-01-30 14:25:01 -0500

    First testing.

--- data_challenge/dataquest2.py
+++ data_challenge/dataquest2.py
@@ -8,23 +8,27 @@ from multiprocessing import Pool
 
 def draw_card(n, trials):
     total_score_of_trials = 0
+    # dictionary of card values
+    dict_card_values = {0:1, 1:2, 2:4, 3:8, 4:16, 5:32, 6:64}
 
     for j in range(trials):
         card_drawn = 0
         card_value = 0
         total = 0
         trial_scores = list()
-        # dictionary of card values
-        dict_card_values = {0:1, 1:2, 2:4, 3:8, 4:16, 5:32, 6:64}
+
         while total < range(n):
             card_drawn = np.random.randint(0,7)
             card_value = dict_card_values[card_drawn]
             total += card_value
+
         trial_scores.append(total - n)
+
     trial_scores = np.array(trial_scores)
     score_mean = np.mean(trials_scores, dtype=np.float64)
     score_SD = np.std(trial_scores, dtype=np.float64)
-    return score_mean, score_SD
+    job_result = [score_mean, score_SD]
+    return job_result
 
 
 
@@ -49,15 +53,17 @@ def main():
     # choice variable for 
     params = [n, repetitions]
     jobs = [] # list of jobs (param inputs) to run through function
-    jobruns = range(1)  # times to repeat same entire job
+    jobruns = range(2)  # times to repeat same entire job
     
     for j in jobruns:
         jobs.append(params)
     
     results = easy_parallize(draw_card, jobs)
+    i = 1
+    for job_result in results:
+        print("Job #", i, "Mean= ", job_result[0], ", SD= ", job_result[1])
+        i += 1
     
-    print("Mean= ", results[0], ", SD= ", results[1])
-            
     print("Total time elapsed for all jobs: ", timeit.default_timer() - total_start_time, " secs")
 
 
